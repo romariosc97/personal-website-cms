92d92c76f5a3367f25fe64acbebd69fefd8ad2d6
romariosc97 <romarito_97@hotmail.com>
2021-08-16 14:19:51 -0500

    init

---
+++ src/views/Home.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import { Box } from "@chakra-ui/react";
+function Home() {
+    return (
+    <div>
+        <h5>SKERE</h5>
+    </div>
+    )
+}
+
+export default Home;
